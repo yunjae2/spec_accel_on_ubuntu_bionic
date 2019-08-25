84292c58f7c30a7e5b2945e07aef901d15afdc21
Yunjae Lee <lyj7694@gmail.com>
2019-08-25 16:11:16 +0900

    run: Set device to CPU explicitly
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ pushd $SPEC_DIR > /dev/null
 
 source ./shrc
 cp config/Example-opencl-intel-cpu.cfg ./config/config.cfg
-logfile=`runspec --iterations 1 --size ref --action onlyrun --config config.cfg --noreportable $workload | grep "The log for this run is in " | awk '{print $8}'`
+logfile=`runspec --device CPU --iterations 1 --size ref --action onlyrun --config config.cfg --noreportable $workload | grep "The log for this run is in " | awk '{print $8}'`
 cat $logfile
 
 popd > /dev/null
